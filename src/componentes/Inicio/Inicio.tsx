bbee788cab4184705e3a271bcc36c3150c4b4448
Mateus Vieira <mateusvieira.duarte@outlook.com>
2025-06-01 18:45:59 -0300

    Iniciando a página inicial

--- src/componentes/Inicio/Inicio.tsx
+++ src/componentes/Inicio/Inicio.tsx
@@ -1,7 +1,27 @@
+import './Inicio.css'
 function Inicio(){
     return(
     <>
-        
+        <div className="container">
+            <div className="inicio-intro">
+            <div className="imagem-inicio">
+                <img src="src\img\vecteezy_cartoon-vegetables-and-fruits_10283782.png" alt="" />
+            </div>
+            <div className="texto">
+            <h2>Olá seja bem-vindo(a) ao Hortifood! 
+                <br />Aqui separamos as melhores frutas, legumes e verduras dos mercados pra vocês!</h2>
+            </div>
+            </div>
+            <div className="inicio-botoes">
+            <div className="botoes">
+                <h3>Cadastre-se como</h3>
+                <button>Cliente</button>
+                <button>Loja</button>
+                <button>Entregador</button>
+                <a href="">Já tenho conta</a>
+            </div>
+            </div>
+        </div>
     </>)
 }
 
